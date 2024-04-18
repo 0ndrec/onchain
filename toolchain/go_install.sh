669e04ed38fd26ef7a617b5e7340946c8a2a097e
Happiness <30905073+0ndrec@users.noreply.github.com>
2024-04-18 17:43:12 +0300

    Create go_install.sh

---
+++ toolchain/go_install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+while getopts v: flag; do
+  case "${flag}" in
+  v) VER=$OPTARG ;;
+  *) echo "WARN: unknown parameter: ${OPTARG}"
+  esac
+done
+
+version=${VER:-"1.20"}
+
+curl -L -O "https://golang.org/dl/go$version.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$version.linux-amd64.tar.gz"
+rm "go$version.linux-amd64.tar.gz"
+
+touch $HOME/.bash_profile
+source $HOME/.bash_profile
+PATH_INCLUDES_GO=$(grep "$HOME/go/bin" $HOME/.bash_profile)
+if [ -z "$PATH_INCLUDES_GO" ]; then
+  echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
+  echo "export GOPATH=$HOME/go" >> $HOME/.bash_profile
+
+  source $HOME/.bash_profile
+  go version
+fi
